f6a7fbd71f0987ad553e39aea66badcdf4d41ea3
Carl Segers <carl.segers@gmail.com>
2022-08-02 11:57:24 -0400

    start converting db scripts to azure sql

--- database/construction.sql
+++ database/construction.sql
@@ -2,52 +2,95 @@ CREATE DATABASE LIBRARY;
 
 USE LIBRARY;
 
+-- CREATE TABLE ACCOUNT (
+--     account_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+--     username VARCHAR(12) NOT NULL UNIQUE,
+--     password VARCHAR(256) NOT NULL
+-- );
+
 CREATE TABLE ACCOUNT (
-    account_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    account_id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
     username VARCHAR(12) NOT NULL UNIQUE,
     password VARCHAR(256) NOT NULL
 );
 
+-- CREATE TABLE EMPLOYEE (
+--     employee_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+--     account_id INT NOT NULL,
+--     FOREIGN KEY (account_id) REFERENCES ACCOUNT(account_id)
+-- );
+
 CREATE TABLE EMPLOYEE (
-    employee_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    employee_id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
     account_id INT NOT NULL,
     FOREIGN KEY (account_id) REFERENCES ACCOUNT(account_id)
 );
 
+-- next statement ok
 CREATE TABLE LIBRARIAN (
     employee_id INT NOT NULL,
     FOREIGN KEY (employee_id) REFERENCES EMPLOYEE(employee_id)
 );
 
+-- next statement ok
 CREATE TABLE BURSAR (
     employee_id INT NOT NULL,
     FOREIGN KEY (employee_id) REFERENCES EMPLOYEE(employee_id)
 );
 
+-- next statement ok
 CREATE TABLE EMPLOYEE_ACCOUNT (
     account_id INT NOT NULL,
     FOREIGN KEY (account_id) REFERENCES ACCOUNT(account_id)
 );
 
+-- CREATE TABLE STUDENT_ACCOUNT (
+--     account_id INT NOT NULL,
+--     FOREIGN KEY (account_id) REFERENCES ACCOUNT(account_id)
+-- );
+
 CREATE TABLE STUDENT_ACCOUNT (
-    account_id INT NOT NULL,
+    account_id INT NOT NULL PRIMARY KEY,
     FOREIGN KEY (account_id) REFERENCES ACCOUNT(account_id)
 );
 
+-- CREATE TABLE LIBRARY_ASSETS (
+--     order_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+--     due_date_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- );
+
 CREATE TABLE LIBRARY_ASSETS (
-    order_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    due_date_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    order_id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+    due_date_time DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
+-- CREATE TABLE FINING_SYSTEM (
+--     fine_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+--     order_id INT NOT NULL,
+--     fee SMALLINT NOT NULL DEFAULT 0,
+--     FOREIGN KEY (order_id) REFERENCES LIBRARY_ASSETS(order_id)
+-- );
+
 CREATE TABLE FINING_SYSTEM (
-    fine_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    fine_id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
     order_id INT NOT NULL,
     fee SMALLINT NOT NULL DEFAULT 0,
     FOREIGN KEY (order_id) REFERENCES LIBRARY_ASSETS(order_id)
 );
 
+-- CREATE TABLE STUDENT (
+--     student_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+--     name VARCHAR(64) NOT NULL,
+--     address VARCHAR(255) NOT NULL,
+--     account_id INT NOT NULL,
+--     rented_book INT NOT NULL DEFAULT 0,
+--     rented_study_room INT NOT NULL DEFAULT 0,
+--     rented_device INT NOT NULL DEFAULT 0,
+--     FOREIGN KEY (account_id) REFERENCES STUDENT_ACCOUNT(account_id)
+-- );
+
 CREATE TABLE STUDENT (
-    student_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    student_id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
     name VARCHAR(64) NOT NULL,
     address VARCHAR(255) NOT NULL,
     account_id INT NOT NULL,
@@ -57,6 +100,7 @@ CREATE TABLE STUDENT (
     FOREIGN KEY (account_id) REFERENCES STUDENT_ACCOUNT(account_id)
 );
 
+-- next statement ok
 CREATE TABLE BORROWS_RETURNS (
     student_id INT NOT NULL,
     order_id INT NOT NULL,
@@ -66,8 +110,21 @@ CREATE TABLE BORROWS_RETURNS (
     FOREIGN KEY (order_id) REFERENCES LIBRARY_ASSETS(order_id)
 );
 
+-- CREATE TABLE BOOK (
+--     book_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+--     title VARCHAR(255) NOT NULL,
+--     author VARCHAR(255) NOT NULL,
+--     physical_location VARCHAR(32) NOT NULL DEFAULT 'Ask Librarian',
+--     publisher VARCHAR(255) NOT NULL,
+--     subject VARCHAR(255) NOT NULL,
+--     publication_date DATE NOT NULL,
+--     language_written_in VARCHAR(2) NOT NULL DEFAULT 'en',
+--     ISBN_13 VARCHAR(13) NOT NULL UNIQUE,
+--     is_rented BOOLEAN NOT NULL DEFAULT false
+-- );
+
 CREATE TABLE BOOK (
-    book_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    book_id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
     title VARCHAR(255) NOT NULL,
     author VARCHAR(255) NOT NULL,
     physical_location VARCHAR(32) NOT NULL DEFAULT 'Ask Librarian',
@@ -76,16 +133,27 @@ CREATE TABLE BOOK (
     publication_date DATE NOT NULL,
     language_written_in VARCHAR(2) NOT NULL DEFAULT 'en',
     ISBN_13 VARCHAR(13) NOT NULL UNIQUE,
-    is_rented BOOLEAN NOT NULL DEFAULT false
+    is_rented BIT NOT NULL DEFAULT 0
 );
+
+-- CREATE TABLE STUDY_ROOM (
+--     room_no INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+--     is_rented BOOLEAN NOT NULL DEFAULT false
+-- );
 
 CREATE TABLE STUDY_ROOM (
-    room_no INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    is_rented BOOLEAN NOT NULL DEFAULT false
+    room_no INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+    is_rented BIT NOT NULL DEFAULT 0
 );
 
+-- CREATE TABLE DEVICE (
+--     device_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+--     type VARCHAR(128) NOT NULL DEFAULT "Unknown",
+--     is_rented BOOLEAN NOT NULL DEFAULT false
+-- );
+
 CREATE TABLE DEVICE (
-    device_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    type VARCHAR(128) NOT NULL DEFAULT "Unknown",
-    is_rented BOOLEAN NOT NULL DEFAULT false
+    device_id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+    type_col VARCHAR(128) NOT NULL DEFAULT 'Unknown',
+    is_rented BIT NOT NULL DEFAULT 0
 );
--- database/user.sql
+++ database/user.sql
@@ -1,3 +1,6 @@
 CREATE USER 'test'@'%' IDENTIFIED BY 'test';
 GRANT ALL PRIVILEGES ON *.* TO 'test'@'%';
 FLUSH PRIVILEGES;
+
+-- Azure SQL version
+-- ask dbas to create a user for you, maybe not named test. I used libadmin
