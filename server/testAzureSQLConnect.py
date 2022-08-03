93892ff00ae8dfa6c22e1399833f0073a709f06a
Carl Segers <carl.segers@gmail.com>
2022-08-03 15:37:42 -0400

    testAzureSQL connect

---
+++ server/testAzureSQLConnect.py
@@ -0,0 +1,17 @@
+import pyodbc
+import os
+
+server = os.getenv("DB_SERVER")
+database = os.getenv("DB_NAME")
+username = os.getenv("DB_USER")
+password = os.getenv("DB_PASS")
+driver= os.getenv("DB_DRIVER")
+port = os.getenv("DB_PORT")
+
+with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT TOP 3 name, collation_name FROM sys.databases")
+        row = cursor.fetchone()
+        while row:
+            print (str(row[0]) + " " + str(row[1]))
+            row = cursor.fetchone()
